36fed15e27edc145ba6aa4175c8919edb7424086
Adriel <Adrie3lmorais@gmail.com>
2026-03-06 12:58:23 -0300

    feat: adicionado DTO de resposta final

--- src/main/java/com/api/materiais/dto/reponse/MaterialResponse.java
+++ src/main/java/com/api/materiais/dto/reponse/MaterialResponse.java
@@ -1,4 +1,12 @@
 package com.api.materiais.dto.reponse;
 
+import lombok.Data;
+import java.util.List;
+
+@Data
 public class MaterialResponse {
+    private String clienteNome;
+    private List<ItemMaterialDTO> materiais;
+    private Double valorTotalEstimado;
+    private String avisoTecnico; // Ex: "Cálculo baseado em estimativa NBR 5410"
 }
