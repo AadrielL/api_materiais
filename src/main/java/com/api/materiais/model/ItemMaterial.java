8bb3d659f4b2ddf63abe169f95bfb2dd8327a8c3
Adriel <Adrie3lmorais@gmail.com>
2026-03-06 12:55:23 -0300

    feat: criadas entidades de banco de dados

--- src/main/java/com/api/materiais/model/ItemMaterial.java
+++ src/main/java/com/api/materiais/model/ItemMaterial.java
@@ -1,4 +1,14 @@
 package com.api.materiais.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class ItemMaterial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descricao;
+    private double quantidade;
 }
--- src/main/java/com/api/materiais/model/Levantamento.java
+++ src/main/java/com/api/materiais/model/Levantamento.java
@@ -1,4 +1,18 @@
 package com.api.materiais.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Entity
+@Data
 public class Levantamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String tenantId;
+    private String clienteNome;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<ItemMaterial> itens;
 }
