5540d693c5e28f8c842c9c9a52d6fcc8522fdc9e
Adriel <Adrie3lmorais@gmail.com>
2026-03-06 12:54:53 -0300

    feat: adicionados DTOs de request e response

--- src/main/java/com/api/materiais/dto/reponse/ItemMaterialDTO.java
+++ src/main/java/com/api/materiais/dto/reponse/ItemMaterialDTO.java
@@ -1,4 +1,12 @@
 package com.api.materiais.dto.reponse;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ItemMaterialDTO {
+    private String descricao;
+    private double quantidade;
+    private String unidade;
 }
--- src/main/java/com/api/materiais/dto/request/LevantamentoRequest.java
+++ src/main/java/com/api/materiais/dto/request/LevantamentoRequest.java
@@ -1,4 +1,13 @@
 package com.api.materiais.dto.request;
 
+import lombok.Data;
+
+@Data
 public class LevantamentoRequest {
+    private String clienteNome;
+    private int qtdQuartos;
+    private int qtdBanheiros;
+    private int qtdCozinhas;
+    private int qtdChuveiros;
+    private boolean incluirArCondicionado;
 }
